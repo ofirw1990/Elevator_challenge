dcf9035d0ed3e4ebdac50eb520e01751e1cb6caa
ofirw1990 <ofirw1990@mail.com>
2024-05-13 16:06:54 +0300

    Reading from a settings file

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
-const numOfBuildings = 6
-const numOfFloors = 12
-const numOfElevators = 5
+const buildings = buildingsData;
 
 const buildingsElement = document.createElement("div");
 buildingsElement.classList.add("buildings");
 
-
-for (let buildingIndex = 0; buildingIndex < numOfBuildings; buildingIndex++) {
-    const newBuilding = BuildingFactory.createBuilding(numOfFloors, numOfElevators);
-    buildingsElement.appendChild(newBuilding.getBuildingElement()); 
-}  
+for (let buildingIndex = 0; buildingIndex < buildings.length; buildingIndex++) {
+  const newBuilding = BuildingFactory.createBuilding(
+    buildings[buildingIndex].numOfFloors,
+    buildings[buildingIndex].numOfElevators
+  );
+  buildingsElement.appendChild(newBuilding.getBuildingElement());
+}
 
 document.body.appendChild(buildingsElement);
-
---
+++ src/settings.ts
@@ -0,0 +1,19 @@
+interface BuildingData {
+  numOfFloors: number;
+  numOfElevators: number;
+}
+
+const buildingsData: BuildingData[] = [
+  {
+    numOfFloors: 10,
+    numOfElevators: 3,
+  },
+  {
+    numOfFloors: 5,
+    numOfElevators: 4,
+  },
+  {
+    numOfFloors: 2,
+    numOfElevators: 5,
+  },
+];
